653c7adc6844518c744253a1b334c14561053ae3
ChathurangaChandrasekara <chathurangajanitha67@gmail.com>
2023-03-06 12:13:42 +0530

    Add project files.

---
+++ WebAPI/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    [Table("Department")]
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+    }
+}
